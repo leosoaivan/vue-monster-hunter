0a0baf3700d2930ded3af131febccde2551ffc88
Leonard Soai-Van <leo.soaivan@gmail.com>
2018-04-25 07:56:10 -0400

    Reset health to 100 on start

--- monster.js
+++ monster.js
@@ -1,6 +1,24 @@
 new Vue({
   el: '#app',
   data: {
-    start: false
+    started: false,
+    fullHealth: 100
+  },
+  methods: {
+    startGame: function() {
+      this.started = !this.started
+    },
+    endGame: function() {
+      this.started = !this.started
+    }
+  },
+  computed: {
+    updateHealth: function() {
+      if(this.started) {
+        return {
+          width: this.fullHealth + '%'
+        }
+      }
+    }
   }
 })
