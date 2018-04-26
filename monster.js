1d46ac68380667e2b3d43baccb2c2fb7f488895f
Leonard Soai-Van <leo.soaivan@gmail.com>
2018-04-25 22:35:20 -0400

    Add attack function

--- monster.js
+++ monster.js
@@ -2,23 +2,32 @@ new Vue({
   el: '#app',
   data: {
     started: false,
-    fullHealth: 100
+    fullHealth: 100,
+    player: {
+      playerHealth: 100
+    },
+    monster: {
+      monsterHealth: 100
+    }
   },
   methods: {
     startGame: function() {
       this.started = !this.started
+      this.player.width = this.fullHealth + '%'
+      this.monster.width = this.fullHealth + '%'
     },
     endGame: function() {
       this.started = !this.started
-    }
-  },
-  computed: {
-    updateHealth: function() {
-      if(this.started) {
-        return {
-          width: this.fullHealth + '%'
-        }
-      }
+    },
+    attack: function() {
+      playerDamage = Math.floor(Math.random() * 10) + 1
+      monsterDamage = Math.floor(Math.random() * 10) + 1
+
+      this.player.playerHealth -= playerDamage
+      this.monster.monsterHealth -= monsterDamage
+
+      this.player.width = this.player.playerHealth + '%'
+      this.monster.width = this.monster.monsterHealth + '%'
     }
   }
 })
