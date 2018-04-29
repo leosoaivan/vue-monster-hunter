a3c5b15390e6168300005aab3d454c5520e3df72
Leonard Soai-Van <leo.soaivan@gmail.com>
2018-04-29 12:18:41 -0400

    Add special attack, heal, and health display

--- monster.js
+++ monster.js
@@ -2,32 +2,50 @@ new Vue({
   el: '#app',
   data: {
     started: false,
+    defaultHealth: 80,
     fullHealth: 100,
     player: {
-      playerHealth: 100
+      health: this.width
     },
     monster: {
-      monsterHealth: 100
+      health: this.width
     }
   },
   methods: {
     startGame: function() {
       this.started = !this.started
+
       this.player.width = this.fullHealth + '%'
+      this.player.health = this.fullHealth
+
       this.monster.width = this.fullHealth + '%'
+      this.monster.health = this.fullHealth
     },
     endGame: function() {
       this.started = !this.started
+
+      this.player.width = this.defaultHealth + '%'
+      this.player.health = this.defaultHealth
+
+      this.monster.width = this.defaultHealth + '%'
+      this.monster.health = this.defaultHealth
     },
-    attack: function() {
+    attack: function(multiplier = 1) {
+      playerDamage = Math.floor(Math.random() * 15) + 1
+      monsterDamage = Math.floor(Math.random() * 15) + multiplier
+      
+      this.player.health -= playerDamage
+      this.monster.health -= monsterDamage
+
+      this.player.width = this.player.health + '%'
+      this.monster.width = this.monster.health + '%'
+    },
+    heal: function() {
+      health = Math.floor(Math.random() * 5) + 1
       playerDamage = Math.floor(Math.random() * 10) + 1
-      monsterDamage = Math.floor(Math.random() * 10) + 1
-
-      this.player.playerHealth -= playerDamage
-      this.monster.monsterHealth -= monsterDamage
-
-      this.player.width = this.player.playerHealth + '%'
-      this.monster.width = this.monster.monsterHealth + '%'
+      
+      this.player.health += (health + playerDamage)
+      this.player.width = this.player.health + '%'
     }
   }
 })
