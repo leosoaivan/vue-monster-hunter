44301425cdb2e72928debbc0e754a926be2a5564
Leonard Soai-Van <leo.soaivan@gmail.com>
2018-05-03 23:49:30 -0400

    Setup Max's solution and implement attack method

--- monster.js
+++ monster.js
@@ -1,93 +1,46 @@
 new Vue({
   el: '#app',
   data: {
-    started: false,
-    defaultHealth: 80,
-    fullHealth: 100,
-    player: {
-      health: this.width
-    },
-    monster: {
-      health: this.width
-    },
-    logs: []
+    gameIsRunning: false,
+    playerHealth: 100,
+    monsterHealth: 100
   },
   methods: {
     startGame: function() {
-      this.started = !this.started
-      this.setHealth(this.fullHealth)
+      this.gameIsRunning = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
     },
-    endGame: function() {
-      this.started = !this.started
-      this.setHealth(this.defaultHealth)
-      this.logs = []
+    attack: function() {
+      var max = 10;
+      var min = 3;
+      var damage = Math.max(Math.floor(Math.random() * max) + 1, min)
+      this.monsterHealth -= damage;
+
+      if (this.monsterHealth <= 0) {
+        alert('You won');
+        this.gameIsRunning = false;
+        return;
+      }
+
+      max = 12;
+      min = 5;
+      var damage = Math.max(Math.floor(Math.random() * max) + 1, min)
+      this.playerHealth -= damage;
+
+      if (this.playerHealth <= 0) {
+        alert('You lost');
+        this.gameIsRunning = false;
+      }
     },
-    attack: function(multiplier = 1) {
-      let damageToPlayer = Math.floor(Math.random() * 15) + 1
-      let damageToMonster = Math.floor(Math.random() * 15) + multiplier
-      
-      this.subtractHealth(this.player, damageToPlayer)
-      this.subtractHealth(this.monster, damageToMonster)
+    specialAttack: function() {
 
-      let playerMessage = "Player hit Monster for " + damageToMonster
-      let monsterMessage = "Monster hit Player for " + damageToPlayer
-
-      this.logs.push([playerMessage, monsterMessage])
-      this.gameResults()
     },
     heal: function() {
-      let damageToPlayer = Math.floor(Math.random() * 15) + 1
-      let healing = Math.floor(Math.random() * 10) + 10
-      
-      this.addHealth(this.player, healing)
-      this.subtractHealth(this.player, damageToPlayer)
 
-      let playerMessage = "Player healed for " + healing
-      let monsterMessage = "Monster hit Player for " + damageToPlayer
-
-      this.logs.push([playerMessage, monsterMessage])
-      this.gameResults()
     },
-    setHealth: function(health) {
-      this.player.health = health
-      this.player.width = health + '%'
+    giveUp: function() {
 
-      this.monster.health = health
-      this.monster.width = health + '%'
-    },
-    subtractHealth: function(character, damage) {
-      character.health -= damage
-
-      if(character.health < 0){
-        character.health = 0
-        character.width = 0 + '%'
-      } else {
-        character.width = character.health + '%'
-      }
-    },
-    addHealth: function(character, healing) {
-      character.health += healing
-
-      if(character.health > 100){
-        character.health = 100
-        character.width = 100 + '%'
-      } else {
-        character.width = character.health
-      }
-    },
-    healthDisplay: function(character) {
-      return character.health
-    },
-    gameResults: function() {
-      if(this.player.health === 0) {
-        alert("Player has been defeated by Monster!")
-        this.endGame()
-      }
-
-      if(this.monster.health === 0) {
-        alert("Player has defeated Monster!")
-        this.endGame()
-      }
     }
   }
 })
